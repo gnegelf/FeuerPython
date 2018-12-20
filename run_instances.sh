2be82e1813c1ad0b6f490faa660e2e34787da466
Fabian Gnegel <gnegel@x8250.math.tu-cottbus.de>
2018-12-20 16:50:57 +0100

    fixed sh scriüt

--- run_instances.sh
+++ run_instances.sh
@@ -1,7 +1,7 @@
 matlab='/Applications/MATLAB_R2017b.app/bin/matlab'
 for i in 10 20
 do 
-$matlab -nodisplay -nojvm -r "addpath('~/MIPDECO/Feuerprojekt');create_instances($i,$i,30,60,0,0); exit;"
+matlab -nodisplay -nojvm -r "addpath('~/MIPDECO/Feuerprojekt');create_instances($i,$i,30,60,0,0); exit;"
 python FeuerVertex.py 0 1 $i $i 30 60
 for j in 30 40 50 60
 do
