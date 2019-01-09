32de3c68ffef7525622fab05c834d10042a8c531
Fabian Gnegel <gnegel@x8250.math.tu-cottbus.de>
2019-01-09 15:58:50 +0100

    Local changes

--- run_instances.sh
+++ run_instances.sh
@@ -1,10 +1,11 @@
 matlab='/Applications/MATLAB_R2017b.app/bin/matlab'
-for i in 10 20
+for i in 3 40 50 60 70 80
 do 
-matlab -nodisplay -nojvm -r "addpath('~/MIPDECO/Feuerprojekt');create_instances($i,$i,30,60,0,0); exit;"
-python FeuerVertex.py 0 1 $i $i 30 60
+matlab -nodisplay -nojvm -r "addpath('~/MIPDECO/Feuerprojekt');create_instances($i,$i,30,60,0,1); exit;"
+python FeuerVertex.py 0 0 $i $i 30 60
 for j in 30 40 50 60
 do
 rm ~/python/data/feuerData${i}_${j}_2.mat
+rm -r ~/MIPDECO/Feuerprojekt/data/matrix*
 done
 done
